ba33f9a70defe06f703046b674435e955408df6a
BJ <bianjin1998@126.com>
2022-10-26 10:48:49 +0800

    first commit

---
+++ load_anno.py
@@ -0,0 +1,7 @@
+import json
+
+json_file = "/media/hkuit164/Backup/ThermalProject/nanodet/workspace/nanodet-plus-m-1.5x_416/20221011170432/results0.json"
+with open(json_file, "r") as load_f:
+    load_dict = json.load(load_f)
+
+    a = 1
